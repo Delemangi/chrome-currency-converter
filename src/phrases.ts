92b3147012ec229395e0cdd2dca9e7aa729ca006
Delemangi <stefan@heckgaming.com>
2021-08-20 18:11:17 +0200

    phrases

--- src/phrases.ts
+++ src/phrases.ts
@@ -1,7 +1,7 @@
 export const selectCurrency: string = "Select a currency.";
 export const rateLimited: string = "Rate limited.";
 export const invalidCurrency: string = "Invalid currency.";
-export const invalidAPIKey: string = "Invalid API key.";
+export const invalidAPIKey: string = "An error has occurred. Please try again.";
 export const cacheTimeNotSet: string = "Caching time is not set.";
 export const historyDaysNotSet: string = "History duration is not set.";
 export const roundDigitsNotSet: string = "Rounding digits is not set.";
@@ -10,7 +10,6 @@ export const unknownError: string = "Unknown error.";
 export const converting: string = "Converting...";
 export const emptyField: string = "Empty field.";
 export const saved: string = "Saved!";
-export const notConfigured: string = "Go to options and configure the extension.";
 
 export const responseInvalidAPICall: string = "Invalid API call. Please retry or visit the documentation";
 export const responseInvalidAPIKey: string = "the parameter apikey is invalid or missing. Please claim your free API key on (https://www.alphavantage.co/support/#api-key). It should take less than 20 seconds.";
